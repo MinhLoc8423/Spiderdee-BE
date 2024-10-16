e042fbc7a2fbea10507afc0a97906b88722d7851
Vũ Minh Lộc <minhlocworkemail@gmail.com>
2024-10-16 11:14:44 +0700

    fix search

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,13 +5,9 @@ const authenticate = require('../middlewares/authMiddleware');
 
 router.get('/products', authenticate, productController.getAllProducts);
 
-<<<<<<< Updated upstream
-router.get('/products/:id', authenticate, productController.getProductById);
-=======
 router.get('/products/search', authenticate, checkPermission("perm_read_product"), productController.searchProducts);
 
 router.get('/products/:id', authenticate, checkPermission("perm_read_product"), productController.getProductById);
->>>>>>> Stashed changes
 
 router.post('/products', authenticate, productController.createProduct);
 
